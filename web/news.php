79794a4841c800204c9ad73d371818825da24a77
ideocl4st <jaeic.lee@gmail.com>
2015-07-20 23:08:47 +0900

    No search yet

--- web/news.php
+++ web/news.php
@@ -1,1 +1,1 @@
-Do not fear, <a href="https://twitter.com/MTGCoverage" target="_new">@MTGCoverage</a>, <a href="https://twitter.com/Ideocl4st" target="_new">I</a> will carry on!
+Do not fear, <a href="https://twitter.com/MTGCoverage" target="_new">@MTGCoverage</a>, <a href="https://twitter.com/Ideocl4st" target="_new">I</a> will carry on! (Advanced Search is not working yet. Sorry!)
