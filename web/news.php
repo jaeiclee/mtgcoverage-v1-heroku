66d19ca5c7baf8c80d18b75f12877862ffb68944
ideocl4st <jaeic.lee@gmail.com>
2015-07-20 21:35:49 +0900

    Silly Stuff

--- web/news.php
+++ web/news.php
@@ -1,1 +1,1 @@
-Follow us on Twitter: <a href="https://twitter.com/MTGCoverage" target="_new">@MTGCoverage</a>
+Do not fear, <a href="https://twitter.com/MTGCoverage" target="_new">@MTGCoverage</a>, <a href="https://twitter.com/Ideocl4st" target="_new">I</a> will carry on!
