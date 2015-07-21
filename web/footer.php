bf9ea884a4eb473360c6228f6aee1b05f35d7d7e
ideocl4st <jaeic.lee@gmail.com>
2015-07-21 19:34:09 +0900

    Changed TCGPlayer logo and copyright stuff

--- web/footer.php
+++ web/footer.php
@@ -1,8 +1,7 @@
 <br><br><br><br><br>
 <div id="main" style="margin:auto; width:800px;">
-<h6>
-The information on this site about Magic: The Gathering, is copyrighted by Wizards of the Coast. This website is not affiliated with Wizards of the Coast.
-</h6><br>
+<h6>Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2015 Wizards. All Rights Reserved. This website is not affiliated with, endorsed, sponsored, or specifically approved by Wizards of the Coast LLC. This website may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under Wizards' Fan Site Policy. MAGIC: THE GATHERING® is a trademark of Wizards of the Coast. For more information about Wizards of the Coast or any of Wizards' trademarks or other intellectual property, please visit their website at www.wizards.com. All other trademarks and logos belong to their respective owners.</h6><br>
 </div></div>
 </body>
 </html>
+
